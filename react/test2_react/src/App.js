aafc277ce8925fc0aaab78c67b2b04c113c853fd
mathieu <mathieu.gaudron@epitech.eu>
2024-08-24 21:03:26 +0200

    [ADD]test2_react OK

---
+++ react/test2_react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Incrémenter ou Décrémenter :) !</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
