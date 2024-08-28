7247a487918aa90a557cc34e40c2c6a96b8bccaa
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-28 13:53:38 +0200

    JS_Task2 DOM interaction

--- javascript/Tasks.js
+++ javascript/Tasks.js
@@ -6,3 +6,14 @@ function getEvenNumbers(numbers) {
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let evenNumbers = getEvenNumbers(array);
 console.log("Even numbers : " + evenNumbers);
+
+// Task 2: DOM Interaction
+function changeTextFunction() {
+  const button = document.getElementById("changeText");
+  const textElement = document.getElementById("myText");
+
+  button.addEventListener("click", () => {
+    textElement.textContent = "Hello, Check!";
+  });
+}
+changeTextFunction();
